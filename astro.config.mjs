07ee54488b3f15e90a4d3c769aec73733f9a851f
alacahanclara-ops <alacahanclara@gmail.com>
2026-06-05 18:28:16 -0300

    config: set GitHub Pages base URL

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://alacahanclara-ops.github.io',
+  base: '/attic-pilates-web/',
+  output: 'static',
+});
