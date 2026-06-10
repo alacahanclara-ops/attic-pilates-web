570a073d05d157310afab9d91cf629d796c58a8e
alacahanclara-ops <alacahanclara@gmail.com>
2026-06-09 21:14:22 -0300

    feat: set custom domain attic.com.ar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://alacahanclara-ops.github.io',
-  base: '/attic-pilates-web/',
+  site: 'https://attic.com.ar',
+  base: '/',
   output: 'static',
 });
